ec6ebf203ad4d1a08553aad36230b05d45ee3a75
Tyler Lutz <tyler@tylerlutz.com>
2018-08-29 10:33:44 -0400

    I Am Rich first commit

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Tyler Lutz on 8/29/18.
+//  Copyright © 2018 Tyler Lutz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
