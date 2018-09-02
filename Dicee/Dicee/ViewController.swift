9499157d4716fc94e2c1406f6cfffb5642c90985
Tyler Lutz <tyler@tylerlutz.com>
2018-09-02 18:02:26 -0400

    Finished Dicee layout

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Tyler Lutz on 8/31/18.
+//  Copyright © 2018 Tyler Lutz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
